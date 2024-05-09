697182688d5459f1e99d4eccb84fedd63c600943
Barath_code <146919684+Barathcode@users.noreply.github.com>
2024-05-09 22:03:49 +0530

    Update Promises.js

--- Promises.js
+++ Promises.js
@@ -29,19 +29,3 @@ const p1 = val => new Promise((resolve,reject)=>{
     },1000);
 })
 p1(1).then(val => val+20).then(console.log) //promise chaining
-
-
-Asynchronous vs Synchronous
-
---->Asynchronous can perform long network requests without blocking the main thread.
---->Synchronous requests can process one at a time, other requests wait until the first one completes.
-
-PROMISES is uses for asynchronous operations
---->introduced in 6th version of JS ES6.
-
-3 States i promises:
--->Pending(still response not come)
--->Fulfilled(response comes)
--->Rejected(reject comes)
-
-To replace callback function promises are introduced.
